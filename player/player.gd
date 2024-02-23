a2bc3177fc7c874e417fb70b43bb9e3db81ddf02
Noah <nordqvist.gaming@gmail.com>
2024-02-23 14:24:35 +0100

    add player health

--- player/player.gd
+++ player/player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
+signal life_changed
+signal died
+
 @export var gravity: float = 750.0
 @export var run_speed: float = 150.0
 @export var jump_speed: float= -300.0
 
 enum State { IDLE, RUN, JUMP, HURT, DEAD }
 var state = State.IDLE
+var life: int: set = set_life
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = $Sprite2D
@@ -24,7 +28,14 @@ func _physics_process(delta) -> void:
         change_state(State.IDLE)
     if state == State.JUMP and velocity.y > 0:
         animation_player.play("jump_down")
-    
+
+
+func set_life(value: int) -> void:
+    life = value
+    life_changed.emit(life)
+    if life <= 0:
+        change_state(State.DEAD)
+
 
 func change_state(new_state: State)  -> void:
     state = new_state
@@ -35,13 +46,22 @@ func change_state(new_state: State)  -> void:
             animation_player.play("run")
         State.HURT:
             animation_player.play("hurt")
+            velocity.y = -200
+            velocity.x = -100 * sign(velocity.x)
+            life -= 1
+            await get_tree().create_timer(0.5).timeout
+            change_state(State.IDLE)
         State.JUMP:
             animation_player.play("jump_up")
         State.DEAD:
+            died.emit()
             hide()
 
 
 func get_input() -> void:
+    if state == State.HURT:
+        return
+        
     var left = Input.is_action_pressed("move_left")
     var right = Input.is_action_pressed("move_right")
     var jump = Input.is_action_just_pressed("jump")
@@ -66,6 +86,12 @@ func get_input() -> void:
 
 
 func reset(_position) -> void:
+    life = 3
     position = _position
     show()
     change_state(State.IDLE)
+
+
+func hurt()  -> void:
+    if state != State.HURT:
+        change_state(State.HURT)
